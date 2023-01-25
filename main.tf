0e68c9a223a0526f46ed3937c2cd5ee127873565
newhopemusician <95931293+newhopemusician@users.noreply.github.com>
2023-01-25 08:33:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
 
-	region = var.region
+	#region = var.region
+	region = "us-east-2"
 
 }
 
@@ -14,7 +15,8 @@ data "aws_ami" "ami_name" {
 }
 
 resource "aws_instance" "web" {
-	ami = data.aws_ami.ami_name.id
+	#ami = data.aws_ami.ami_name.id
+	ami = ami-02fe99ea40f8d73fe
 	subnet_id = aws_subnet.publicsubnet.id
 	instance_type = "t2.micro"
 
