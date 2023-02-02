b96b9731bae34a695ebbf23b38d0ed0e5ab94042
simpsonhomerjay <124053675+simpsonhomerjay@users.noreply.github.com>
2023-02-02 07:57:19 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "ami" {
+	default = "ami-0471021a4ee6bfcce"
+}
+
+variable "region" {
+	default = "us-east-2"
+}
+
+variable "insttype" {
+	default = "t2.small"
+}
+
 variable "main_vpc_cidr" {
 	default = "10.0.0.0/24"
 }
@@ -10,10 +22,5 @@ variable "private_subnets" {
 	default = "10.0.0.192/26"
 }
 
-variable "region" {
-	default = "us-east-2"
-}
 
-variable "ami" {
-	default = ""
-}
+
